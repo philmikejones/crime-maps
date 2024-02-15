2036533d6bc6e8791fe6f4c4346a9fd105e52676
Phil Mike Jones <philmikejones@gmail.com>
2024-02-15 23:03:14 +0000

    map only 2023 data

--- 01-process-crime-data.R
+++ 01-process-crime-data.R
@@ -7,7 +7,7 @@ normanton =
     sf::read_sf("data/normanton-neighbourhood.shp") %>%
     st_transform(crs = st_crs("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
 
-csvs = list.files("data/normanton", recursive = TRUE, full.names = TRUE, pattern = ".csv")
+csvs = list.files("data/normanton/2023", recursive = TRUE, full.names = TRUE, pattern = ".csv")
 normanton_crimes = lapply(csvs, readr::read_csv)
 normanton_crimes = bind_rows(normanton_crimes)
 
