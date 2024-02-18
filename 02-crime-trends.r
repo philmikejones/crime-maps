fa0e4fdc4667f0f6156f73f04c9fa6e4ae620b06
Phil Mike Jones <philmikejones@gmail.com>
2024-02-18 10:55:19 +0000

    plot burglary trends

--- 02-crime-trends.r
+++ 02-crime-trends.r
@@ -1,5 +1,7 @@
 library("dplyr")
+library("lubridate")
 library("sf")
+library("ggplot2")
 
 normanton =  # to clip crimes to normanton ward
     sf::read_sf("data/normanton-neighbourhood.shp") %>%
@@ -22,15 +24,12 @@ n2023 = mutate(n2023, year = 2023)
 
 normanton_crimes = bind_rows(n2021, n2022, n2023)
 
-print(normanton_crimes)
-
-stop()
-
 normanton_crimes = 
     normanton_crimes %>%
     mutate(id = row_number()) %>%
     select(
         id,
+        year,
         Month,
         Longitude,
         Latitude,
@@ -42,8 +41,32 @@ normanton_crimes =
         lat = `Latitude`,
         type = `Crime type`
     ) %>%
+    mutate(month = paste0(month, "-01")) %>%
+    mutate(month = lubridate::as_date(month)) %>%
     filter(!is.na(long), !is.na(lat)) %>%
     st_as_sf(coords = c("long", "lat")) %>%
     st_set_crs("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")    
 
 normanton_crimes = filter(normanton_crimes, st_within(normanton_crimes, normanton, sparse = FALSE))
+
+print(unique(normanton_crimes$type))
+
+types = unique(normanton_crimes$type)
+
+b = normanton_crimes[normanton_crimes$type == "Burglary", ]
+
+b = b[b$year == 2021, ]
+
+b = as_tibble(b)
+
+b = count(b, month)
+print(b)
+
+p = 
+    ggplot(b, aes(month, n)) + 
+    geom_line() +
+    labs(x = "Month", y = "Number of offences", caption = "Burglary") +
+    scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y") +  # https://stackoverflow.com/a/42929948
+    theme(axis.text.x=element_text(angle = 60, hjust = 1))
+
+ggsave(p, width = 297, height = 210, units = "mm", file = "burglary-trend.pdf")
