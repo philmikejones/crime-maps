d51c7c45c737045fc2b69521cf42bc00c3d3b143
Phil Mike Jones <philmikejones@gmail.com>
2024-02-19 15:42:42 +0000

    plot crime trends

--- 02-crime-trends.r
+++ 02-crime-trends.r
@@ -37,19 +37,25 @@ crimes = filter(crimes, st_within(crimes, normanton, sparse = FALSE))
 
 types = unique(crimes$type)
 
-b = crimes[crimes$type == "Burglary", ]
-b = as_tibble(b)
-b = count(b, month)
+for(type in types) {
+    dat = crimes[crimes$type == type, ]
+    dat = as_tibble(dat)
+    dat = count(dat, month)
 
-p = 
-    ggplot(b, aes(month, n)) + 
-    geom_vline(xintercept = ymd('2021-01-01'), linetype = "dashed") +
-    geom_vline(xintercept = ymd('2022-01-01'), linetype = "dashed") +
-    geom_vline(xintercept = ymd('2023-01-01'), linetype = "dashed") +
-    geom_line() +
-    geom_smooth(method = "loess", formula = "y ~ x") +
-    labs(x = "Month", y = "Number of offences", caption = "Burglary") +
-    scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y") +  # https://stackoverflow.com/a/42929948
-    theme(axis.text.x=element_text(angle = 60, hjust = 1))
+    p = 
+        ggplot(dat, aes(month, n)) + 
+        geom_vline(xintercept = ymd('2021-01-01'), linetype = "dashed") +
+        geom_vline(xintercept = ymd('2022-01-01'), linetype = "dashed") +
+        geom_vline(xintercept = ymd('2023-01-01'), linetype = "dashed") +
+        geom_line() +
+        geom_smooth(method = "loess", formula = "y ~ x") +
+        labs(x = "Month", y = "Number of offences", caption = type) +
+        scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y") +  # https://stackoverflow.com/a/42929948
+        theme(axis.text.x=element_text(angle = 60, hjust = 1))
 
-ggsave(p, width = 297, height = 210, units = "mm", file = "burglary-trend.pdf")
+    ggsave(
+        p, width = 297, height = 210, units = "mm",
+        file = paste0("docs/normanton/", type, "-trend.pdf"), title = paste0(type, " trend, Normanton, 2021-2023")
+    )
+
+}
